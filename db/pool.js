934ac1024af975385a588b61216380c6bb83fe04
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-11 11:24:44 +0100

    fix the variables name

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 module.exports = new Pool({
-  connectionString: `postgresql://process.env.DB_USER:process.env.DB_PASSWORD@process.env.DB_HOST:process.env.DB_PORT/process.env.DB_NAME`
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 });
