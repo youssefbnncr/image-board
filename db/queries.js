c3ac5cfb27a6858c29b00ffc8cdddb79b406369e
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-11 11:24:13 +0100

    fix the name of the table

--- db/queries.js
+++ db/queries.js
@@ -1,13 +1,17 @@
 const pool = require('./pool');
 
 const checkUserExists = async (email) => {
-    const result = await pool.query('SELECT * FROM "Users" WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM "users" WHERE email = $1', [email]);
     return result.rows.length > 0;
 };
 
 const insertUser = async (first_name, last_name, email, password, membership_status) => {
-    const query = 'INSERT INTO "Users" (first_name, last_name, email, password, membership_status) VALUES ($1, $2, $3, $4, $5)';
-    await pool.query(query, [first_name, last_name, email, password, membership_status]);
+    const query = 'INSERT INTO "users" (first_name, last_name, email, password, membership_status) VALUES ($1, $2, $3, $4, $5)';
+    try {
+        await pool.query(query, [first_name, last_name, email, password, membership_status]);
+    } catch (error) {
+        console.error('Error inserting user:', error);
+    }
 };
 
 module.exports = {
