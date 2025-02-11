7c6ed3fde613242066bc7a94b850903acc3ee6c9
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-11 11:25:54 +0100

    fix the flashjs

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,13 +3,13 @@ const userRouter = express.Router();
 const userController = require('../controllers/userController');
 
 userRouter.get('/user/signup', (req, res) => {
-    res.render('signup', { oldData: {} });
+    res.render('signup', { oldData: {}, messages: req.flash() });
 });
 
 userRouter.post('/user/signup', userController.signup);
 
 userRouter.get('/user/login', (req, res) => {
-  res.render('login');
+    res.render('login', { messages: req.flash() });
 });
 
 module.exports = userRouter;
