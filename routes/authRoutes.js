68b887a9fd10664599ab8a0c0141da9ee8d65eb6
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-13 12:04:27 +0100

    fix handling some routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const authRouter = express.Router();
+const authController = require('../controllers/authController');
+const { ensureAuthenticated, forwardAuthenticated } = require('../middleware/authMiddleware');
+
+authRouter.get('/', ensureAuthenticated, (req, res) => {
+  res.render('home', { user: req.user });
+});
+
+authRouter.get('/signup', forwardAuthenticated, (req, res) => {
+  res.render('signup', { oldData: {}, messages: {} });
+});
+
+authRouter.post('/signup', authController.signup);
+
+authRouter.get('/login', forwardAuthenticated, (req, res) => {
+  res.render('login', { messages: {} });
+});
+
+authRouter.post('/login', authController.login);
+
+authRouter.get('/dashboard', ensureAuthenticated, (req, res) => {
+  res.render('dashboard', { user: req.user });
+});
+
+authRouter.get('/logout', authController.logout);
+
+module.exports = authRouter;
