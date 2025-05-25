62bf9e9a7e4193d4c223b3b63c464782685aa14a
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-05-25 17:17:49 +0100

    setup bcrypt for encreption

--- model/queries.js
+++ model/queries.js
@@ -1,9 +1,11 @@
 const pool = require('./pool')
+const bcrypt = require('bcryptjs');
 
 const signup = async(username, password) => {
     try {
-    await pool.query("INSERT INTO users (username, password) VALUES (\$1, \$2)",[username,password])
-    console.log('New account has been created')
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query("INSERT INTO users (username, password) VALUES (\$1, \$2)",[username,hashedPassword])
+        console.log('New account has been created')
     } catch(e) {
         console.error("Error creating user:",e);
     }
