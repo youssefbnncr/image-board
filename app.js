d4399cee2873c35f50991c5c25cca188c9abf1c7
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-05-27 18:14:10 +0100

    check if the user is loged or not

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ const assetsPath = path.join(__dirname, "src/css");
 app.use(express.static(assetsPath));
 
 app.use('/user/',userRouter);
-app.get('/',(req,res)=>res.render("index"))
-
+app.get('/',(req,res)=>res.render("index",{isLoged:req.isAuthenticated()}))
 
 app.listen(3000, () => console.log(`Server is running at: http://localhost:3000`));
