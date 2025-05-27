3974ef05e666642b990715a788d9bcfebc659897
Edios44 <161240985+Edios44@users.noreply.github.com>
2025-05-27 19:18:42 +0100

    fix type error

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,6 +9,6 @@ userRouter.get("/log-out", user.logout);
 
 userRouter.post('/add-user', signupValidation, user.addUser);
 userRouter.post('/userLogin', user.login);
-userRouter.port('/log-out', user.logout);
+userRouter.post('/log-out', user.logout);
 
 module.exports = userRouter;
