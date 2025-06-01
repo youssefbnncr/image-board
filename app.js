c28a58ec5d1c109de5dd1f83e652f9b0a6452f3f
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-06-01 19:58:53 +0100

    organize

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const path = require("node:path");
 const express = require("express");
 const passport = require("passport");
-const session = require('express-session');
+const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
-const flash = require('connect-flash');
+const flash = require("connect-flash");
 
-const pool = require('./model/pool')
-const initializePassport = require('./config/passportConfig');
-require('dotenv').config();
+const pool = require("./model/pool");
+const initializePassport = require("./config/passportConfig");
+require("dotenv").config();
 
-const userRouter = require('./routes/userRouter')
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
 initializePassport(passport);
-app.use(session({
+app.use(
+  session({
     store: new pgSession({
       pool: pool,
     }),
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
     rolling: true,
- 	secret: `${process.env.session_password}`,
-	resave: false,
-	saveUninitialized: false,
-}));
+    secret: `${process.env.session_password}`,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 app.use(passport.session());
 app.use(flash());
@@ -36,21 +38,25 @@ app.use(express.urlencoded({ extended: false }));
 const assetsPath = path.join(__dirname, "src/css");
 app.use(express.static(assetsPath));
 
-app.use('/uploads/avatars', express.static(path.join(__dirname, 'uploads/avatars')));
-
-
-app.use((req,res,next)=>{
-  res.locals.error_messages = req.flash('error');
-  next();
-})
+app.use(
+  "/uploads/avatars",
+  express.static(path.join(__dirname, "uploads/avatars")),
+);
 
 app.use((req, res, next) => {
-    res.locals.isLoged = req.isAuthenticated();
-    res.locals.user = req.user;
-    next();
+  res.locals.error_messages = req.flash("error");
+  next();
 });
 
-app.use('/user/',userRouter);
-app.get('/',(req,res)=>res.render("index"))
+app.use((req, res, next) => {
+  res.locals.isLoged = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
 
-app.listen(3000, () => console.log(`Server is running at: http://localhost:3000`));
+app.use("/user/", userRouter);
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () =>
+  console.log(`Server is running at: http://localhost:3000`),
+);
