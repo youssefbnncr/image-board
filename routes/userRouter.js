32f35f5de6308408a7e8b1533a1317572f932f18
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-06-04 20:05:07 +0100

    setup admin view, add queries for quering the users and categories, check permission before accessing the admin panel

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,12 +3,14 @@ const { signupValidation } = require("../model/queries");
 const { Router } = require("express");
 const userRouter = Router();
 const upload = require("../config/multerConfig");
+const { isAdmin } = require("../config/authConfig");
 
 userRouter.get("/register", user.signup);
 userRouter.get("/login", user.signin);
 userRouter.get("/log-out", user.logout);
 userRouter.get("/user-settings", user.settings);
 userRouter.get("/verify", user.getVerify);
+userRouter.get("/admin", isAdmin, user.admin);
 
 userRouter.post("/add-user", signupValidation, user.addUser);
 userRouter.post("/userLogin", user.login);
