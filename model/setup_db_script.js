88bbe584edfa59222e7ba3fdc25af85f245b2b87
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-06-04 20:03:51 +0100

    update table list

--- model/setup_db_script.js
+++ model/setup_db_script.js
@@ -14,39 +14,34 @@ CREATE TABLE IF NOT EXISTS users (
   role INT DEFAULT 0 -- 0: Default, 1: Moderator, 2: Admin
 );
 
+-- Categories Table
+CREATE TABLE IF NOT EXISTS categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
 -- Boards Table
 CREATE TABLE IF NOT EXISTS boards (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE,
+  category_id INT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  created_by INT REFERENCES users(id) -- Admin creates the boards
-);
-
--- Categories Table
-CREATE TABLE IF NOT EXISTS categories (
-  id SERIAL PRIMARY KEY,
-  board_id INT REFERENCES boards(id) ON DELETE CASCADE,
-  name VARCHAR(255) NOT NULL
-);
-
--- Subcategories Table (For storing items under each category like "Phone Games", "Sports", etc.)
-CREATE TABLE IF NOT EXISTS subcategories (
-  id SERIAL PRIMARY KEY,
-  category_id INT REFERENCES categories(id) ON DELETE CASCADE,
-  name VARCHAR(255) NOT NULL
+  created_by INT REFERENCES users(id)
 );
 
 -- Threads Table
 CREATE TABLE IF NOT EXISTS threads (
   id SERIAL PRIMARY KEY,
-  subcategory_id INT REFERENCES subcategories(id) ON DELETE CASCADE,
+  board_id INT REFERENCES boards(id) ON DELETE CASCADE,
   user_id INT REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
+  image TEXT,
+  message TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Posts Table (This will be for both threads and replies)
-CREATE TABLE IF NOT EXISTS posts (
+-- Replies Table
+CREATE TABLE IF NOT EXISTS replies (
   id SERIAL PRIMARY KEY,
   thread_id INT REFERENCES threads(id) ON DELETE CASCADE,
   user_id INT REFERENCES users(id),
@@ -67,11 +62,10 @@ async function main() {
   await client.query(SQL);
 
   console.log("Users Table created.");
-  console.log("Boards Table created.");
   console.log("Categories Table created.");
-  console.log("Subcategories Table created.");
+  console.log("Boards Table created.");
   console.log("Threads Table created.");
-  console.log("Posts Table created.");
+  console.log("Replies Table created.");
 
   await client.end();
   console.log("All tables created successfully.");
