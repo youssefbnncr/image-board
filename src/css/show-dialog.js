367086357a21471f7feb5b2d68fe4e14bade841c
Edios44 <161240985+Edios44@users.noreply.github.com>
2025-06-04 19:21:53 +0100

    added a form for making threads

---
+++ src/css/show-dialog.js
@@ -0,0 +1,6 @@
+const showDialog = document.querySelector("#show-create-thread-form");
+const dialog = document.querySelector(".dialog");
+
+showDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
