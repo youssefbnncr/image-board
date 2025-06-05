450f617652219c431c4a9a290adfa4559b29a54b
youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-06-05 19:12:01 +0100

    display boards page.

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -1,6 +1,8 @@
 const db = require("../model/queries");
 
 const displayBoard = async (req, res) => {
+  res.locals.categories = await db.getCategories();
+  res.locals.boards = await db.getBoards();
   res.render("board");
 };
 module.exports = { displayBoard };
